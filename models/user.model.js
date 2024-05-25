f9e9d20339c27d00ea691051a895ecb2d89d2592
Uthman Oluwaseun <amoleuthman@gmail.com>
2024-05-25 15:02:37 +0100

    wahala wahala

--- models/user.model.js
+++ models/user.model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 userSchema = mongoose.Schema(
     {
         fullName: {type:String, required:true, },
-        email: {type:String, required:true, unique:true },
+        email: {type:String, required:true, unique:true},
         password: {type:String, required:true, }
     }
 )
